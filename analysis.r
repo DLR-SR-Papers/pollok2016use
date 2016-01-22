9972b978e61c58a9ea8005557ce42ab76bfba065
Alexander Pollok <alexander.pollok@gmail.com>
2016-01-22 15:42:58 +0100

    add analysis files

---
+++ analysis.r
@@ -0,0 +1,56 @@
+
+# RUN: source("analysis.r")
+
+# Load data
+dirname     = dirname(sys.frame(1)$ofile)
+filename    = paste(dirname,"/investigation.csv", sep="")
+logname     = paste(dirname,"/analysis.log", sep="")
+outname     = paste(dirname,"/analysis.csv", sep="")
+data        = read.csv(filename, sep=";", dec=",")
+data$simp   = data$eq/data$sym
+
+# All output also to logfile
+sink(logname, split=TRUE)
+
+# Apply filters to the data
+filt1       = subset(data,  vp != "V8b")   
+filt2       = subset(filt1, filter < 0.5)
+
+# Run some descriptive statistics
+library(psych)
+cat("\nDescriptive statistics of considered cases\n")
+print(describe(filt2))
+cat("\nDescriptive statistics of means of considered subjects\n")
+print(describe(aggregate(cbind(exp, time) ~ vp, FUN=mean, data=filt1)))
+
+# Check correlations of independents
+cat("\nCorrelations of independents\n")
+print(cor(cbind(filt2$sym, filt2$eq, filt2$lvls)))
+
+# Check that experience has an influence
+cat("\nTesting for significant exp*sym effects\n")
+lmExp = lm(time ~ exp*sym, data=filt2)
+print(summary(lmExp))
+
+# Create regression object with repeated measures (mean and sym-slopes per subject)
+library(nlme)
+lmNull      = lme(           time ~ 1,           random = ~(0+sym) | vp, data = filt2, method = "ML")
+lmLvls      = update(lmNull, time ~ 1+lvls)
+
+# Output summaries
+cat("\nCoefficients of model with levels\n")
+print(coef(lmLvls))
+
+# Comparison of models
+cat("\nSignificance tests\n")
+print(summary(lmLvls))
+
+# Plot what
+data2 = aggregate(cbind(exp, time) ~ vp, FUN=mean, data=filt1)
+rownames(data2) = data2$vp
+data2 = data.frame(data2, coef(lmLvls))
+plot(data2$exp, data2$sym)
+write.table(data2, file=outname, sep=";", dec=".", row.names=FALSE, quote=FALSE)
+
+# Reset output redirection
+sink()
